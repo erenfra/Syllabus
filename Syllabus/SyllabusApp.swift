b899c8d542c38b12cff2ac0448d58bac428224d0
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-30 22:00:53 -0600

    Initial Commit

---
+++ Syllabus/SyllabusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyllabusApp.swift
+//  Syllabus
+//
+//  Created by Renato Oliveira Fraga on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SyllabusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
